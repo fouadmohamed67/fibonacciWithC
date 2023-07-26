a9f1f39b696a6a7e88cdba948576d8650cf6ba05
fouadmohamed67 <fouadmohamedfouad67@gmail.com>
2023-07-26 13:09:23 +0300

    fix and add:fix some bugs in the code make it accept Node, and add fibonanci function to the whole solution, also add calc function that get Node type and the value then print them

--- QuestionNo.1,2/QuestionNo.1/QuestionNo.1/QuestionNo.1/solution.c
+++ QuestionNo.1,2/QuestionNo.1/QuestionNo.1/QuestionNo.1/solution.c
@@ -5,12 +5,12 @@ typedef enum TypeTag {
     ADD,
     SUB,
     MUL,
+    FIBO
 } TypeTag;
 
 typedef struct Node {
     TypeTag type;
-    int data;
-    struct Node* next;
+    int data; 
 } Node;
 
 // Function prototypes
@@ -26,6 +26,15 @@ Node* makeFunc(TypeTag op, Node *a, Node *b) {
         return subFunc(a->data, b->data);
     case MUL: 
         return mulFunc(a->data, b->data);
+    case FIBO:
+    {
+        int fibResult = fibFun(a->data);
+        Node* node = (Node*)malloc(sizeof(Node));
+        node->type = FIBO;
+        node->data = fibResult;
+      
+        return node;
+    } 
     default:
         return NULL;
     }
@@ -34,40 +43,69 @@ Node* makeFunc(TypeTag op, Node *a, Node *b) {
 Node* addFunc(int a, int b) {
     Node* node = (Node*)malloc(sizeof(Node));
     node->type = ADD;
-    node->data = a + b;
-    node->next = NULL;
+    node->data = a + b; 
     return node;
 }
 
 Node* subFunc(int a, int b) {
     Node* node = (Node*)malloc(sizeof(Node));
     node->type = SUB;
-    node->data = a - b;
-    node->next = NULL;
+    node->data = a-b; 
     return node;
 }
 
 Node* mulFunc(int a, int b) {
     Node* node = (Node*)malloc(sizeof(Node));
     node->type = MUL;
-    node->data = a * b;
-    node->next = NULL;
+    node->data = a * b; 
     return node;
 }
+int fibFun(int a) {
+    if (a == 0) {
+        return 0;
+    }
+    else if (a== 1) {
+        return 1;
+    }
+    else {
+        return fibFun(a - 1)  + fibFun(a - 2);
+    }
+}
+void calc(Node* node) {
+    switch (node->type) {
+    case 0:
+        return   printf("add = %d\n", node->data);
+    case 1:
+        return  printf("sub = %d\n", node->data);
+    case 2:
+        return  printf("mul = %d\n", node->data);
+    case 3:
+        return  printf("FIBO = %d\n", node->data);
 
+    default:
+        return NULL;
+    }
+    
+}
 int main() {
     Node* d1= (Node*)malloc(sizeof(Node));
     d1->data = 10;
     Node* d2 = (Node*)malloc(sizeof(Node));
-    d2->data = 6;
+    d2->data = 6; 
+    Node* d3 = (Node*)malloc(sizeof(Node));
+    d3->data = 5;
+    Node* d4 = (Node*)malloc(sizeof(Node));
+    d4->data = 4;
 
-    Node* add = makeFunc(ADD, d1, d2);
-    Node* mul = makeFunc(MUL, d1, d2);
-    Node* sub = makeFunc(SUB, mul, add);
-    printf("add: %d\n", add->data);
-    printf("mul: %d\n", mul->data);
-    printf("sub: %d\n", sub->data); 
+   Node* add = makeFunc(ADD, d1, d2);
+   Node* mul = makeFunc(MUL, d3, d4);
+   Node* sub = makeFunc(SUB, mul, add);
+   Node* fibo = makeFunc(FIBO, sub, NULL);
+   calc(add);
+   calc(mul);
+   calc(sub);
+   calc(fibo);
 
-    free(add);
+     
     return 0;
 }
